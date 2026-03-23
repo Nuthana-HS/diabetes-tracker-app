82aadc7475464af73388c21fa0b5ad99a970f8df
Nuthana HS <nuthanahs86@gmail.com>
2026-03-23 14:35:39 +0530

    Set @secret as global variable in environment.rb

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+# Set global secret before Rails initializes
+$secret = ENV['SECRET'] || 'e05de8d13df0bd61'
+@secret = $secret
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- backend/config/initializers/00_secret_key.rb
+++ backend/config/initializers/00_secret_key.rb
@@ -3,5 +3,9 @@ ENV['SOLID_QUEUE_ENCRYPTION_KEY'] ||= 'e05de8d13df0bd61'
 ENV['ENCRYPTION_KEY'] ||= 'e05de8d13df0bd61'
 ENV['SECRET'] ||= 'e05de8d13df0bd61'
 
-# Set @secret directly (this is what the error is looking for)
-@secret = ENV['SECRET'] || ENV['ENCRYPTION_KEY'] || 'e05de8d13df0bd61'
+# Set @secret as a global variable (accessible everywhere)
+$secret = ENV['SECRET'] || ENV['ENCRYPTION_KEY'] || 'e05de8d13df0bd61'
+@secret = $secret  # Also set instance variable for compatibility
+
+# Set as a constant
+SECRET_KEY = $secret
