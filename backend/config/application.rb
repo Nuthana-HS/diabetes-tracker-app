177936b35d716d9edca37b728439f640da9aec8b
Nuthana HS <nuthanahs86@gmail.com>
2026-03-23 14:41:38 +0530

    Disable ActiveRecord Encryption to fix @secret error

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -40,5 +40,8 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Disable ActiveRecord Encryption
+   config.active_record.encryption.enabled = false
   end
 end
